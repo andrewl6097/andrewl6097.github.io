13dcc25c463045094e164ae53e7e3b24da6f9d53
Andrew Lusk <andrewlu@amazon.com>
2021-12-05 13:02:56 -0800

    Initial site checkin.

---
+++ _posts/2021-12-05-first-post.md
@@ -0,0 +1,15 @@
+---
+layout: post
+title: 'First post'
+date: '2021-12-05 12:42:29 -0800'
+categories: []
+comments: false
+---
+This is a first test post.
+
+Some markdown listing:
+
+* this
+* is
+* a
+* list
---
+++ tools/upload.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+set -x
+
+bundle exec jekyll b
+cp -av img/ _site/assets/img/
+
+aws s3 sync _site s3://run-parallel.sh/
+aws cloudfront create-invalidation --distribution-id EO0JOSZAC367N --paths /index.html /feed.xml /categories/index.html /posts/index.html /tags/index.html /archives/index.html
